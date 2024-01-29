2d8368a767a154fad8726f53d5a0487dac50c29c
eziraa <ezratgab@gmail.com>
2024-01-29 03:28:51 +0300

    necessary element selected and stored on thier variable

---
+++ script.js
@@ -0,0 +1,12 @@
+// Delcare document elment variable
+
+const password = document.querySelector(".password");
+const btnCopy = document.querySelector(".copy");
+const strong_level = document.querySelector("#strongness");
+const password_range = document.querySelector("#length-range");
+const password_length = document.querySelector(".pass-length-text");
+const includeLower = document.querySelector(".lower-checkbox");
+const includeUpper = document.querySelector(".upper-checkbox");
+const includeSymbols = document.querySelector(".symbols-checkbox");
+const includeNumbers = document.querySelector(".numbers-checkbox");
+const btnGenerate = document.querySelector(". btn-generate");
